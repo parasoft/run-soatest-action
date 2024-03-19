442247684cceba92ea45200ad0d7fcd3e0bda221
tren <tren@parasoft.com>
2024-03-19 12:04:46 +0800

    Introduce test framework.

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,21 @@
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
 
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.add = void 0;
 console.log("Hello, World!");
+// Add this function for introduce test framework and will be removed in next pull request
+function add(a, b) {
+    return a + b;
+}
+exports.add = add;
 //# sourceMappingURL=main.js.map
+})();
+
 module.exports = __webpack_exports__;
 /******/ })()
 ;
